6a426d75972848f8dcb3f6cb9b7f57e13207086e
redpangilinan <redpangilinan715@gmail.com>
2023-12-20 21:11:39 +0800

    refactor(loaders): adjust layout for sort button

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { Navbar } from "@/components/layout/navbar"
 import { JobCardSkeleton } from "@/components/loaders/job-card-skeleton"
 
@@ -6,10 +7,15 @@ export default async function Home() {
     <>
       <Navbar />
       <section className="container grid items-start gap-12 p-8 md:grid-cols-[1fr,20rem]">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          {[...Array(8)].map((_, index) => (
-            <JobCardSkeleton key={index} />
-          ))}
+        <div className="space-y-4">
+          <div className="flex justify-end">
+            <Button variant={"outline"}>Sort by</Button>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            {[...Array(8)].map((_, index) => (
+              <JobCardSkeleton key={index} />
+            ))}
+          </div>
         </div>
         <aside className="space-y-4 border-l border-gray-200 p-6">
           <h2 className="text-xl font-bold">Discover internships</h2>
--- src/components/loaders/job-card-skeleton.tsx
+++ src/components/loaders/job-card-skeleton.tsx
@@ -1,21 +1,13 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export function JobCardSkeleton() {
   return (
     <Card>
       <div>
-        <CardHeader className="space-y-1">
+        <CardHeader className="space-y-2">
           <Skeleton className="h-[1.5rem] w-[18rem]" />
-          <CardDescription>
-            <Skeleton className="h-[1rem] w-[12rem]" />
-          </CardDescription>
+          <Skeleton className="h-[1rem] w-[12rem]" />
         </CardHeader>
         <CardContent className="space-y-2">
           <Skeleton className="h-[1rem] w-full" />
